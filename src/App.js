10e7d86be6af64afc743782cf412537037c15bdb
SashaB <65488004+SashaBasile@users.noreply.github.com>
2020-11-07 11:36:55 +0000

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+//this component is the parent of the menu component as it uses its data
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // the state stores data relative to this component
+    // which you can make use of
+    // you can pass these information to any child component via props
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
