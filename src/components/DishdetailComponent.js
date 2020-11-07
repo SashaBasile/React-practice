1c830212103a88d0ccedfe39f3a6159edb97d30d
SashaB <65488004+SashaBasile@users.noreply.github.com>
2020-11-07 15:22:36 +0000

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,81 +1,71 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 
-class DishDetail extends React.Component {
+function RenderDish({ dish }) {
+    //const dish = this.props.selectedDish;
+    return (
+        <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
-    renderDish(dish) {
-        //const dish = this.props.selectedDish;
+function RenderComments({ dish }) {
+    if (dish != null) {
+        const coms = dish.comments.map((com) => {
+            return (
+                <ul key={com.id} className='list-unstyled'>
+                    <li>
+                        {com.comment}
+                    </li>
+                    <li>
+                        -- {com.author}, { }
+                        {
+                            new Intl.DateTimeFormat('en-US', {
+                                month: 'short', day: '2-digit', year: 'numeric'
+                            }).format(new Date(com.date))
+                        }
+                    </li>
+                </ul>
+            );
+        });
 
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div> //if you return an empty div, nothing will be rendered on the screen
-            );
-        }
+        return (
+            <div>
+                <h4>Comments</h4>
+                {coms}
+            </div>
+        );
+
+    } else {
+        return (
+            <div></div>
+        );
     }
+}
 
-    renderComments(dish) {
-        if (dish != null) {
-            const coms = dish.comments.map((com) => {
-                return (
-                    <ul key={com.id} className='list-unstyled'>
-                        <li>
-                            {com.comment}
-                        </li>
-                        <li>
-                            -- {com.author}, { }
-                            {
-                                new Intl.DateTimeFormat('en-US', {
-                                    month: 'short', day: '2-digit', year: 'numeric'
-                                }).format(new Date(com.date))
-                            }
-                        </li>
-                    </ul>
-                );
-            });
-
-            return (
-                <div>
-                    <h4>Comments</h4>
-                    {coms}
-                </div>
-            );
-
-        } else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
-
-
-    render() {
-        const dish = this.props.selectedDish;
-
+const DishDetail = (props) => {
+    if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(dish)}
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.selectedDish)}
+                        <RenderComments dish={props.selectedDish} />
                     </div>
                 </div>
             </div>
-
         )
+    } else {
+        return (
+            <div></div> //if you return an empty div, nothing will be rendered on the screen
+        );
     }
 }
 
