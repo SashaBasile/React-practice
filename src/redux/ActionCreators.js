f869f5112c5481d2c247343e1c06e5de9977258e
SashaB <65488004+SashaBasile@users.noreply.github.com>
2020-11-25 18:15:56 +0000

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,7 @@
 import * as ActionTypes from './ActionTypes';
 // * means everything which has been exported from a given file
+import { DISHES } from '../shared/dishes';
+
 
 //Action object 
 export const addComment = (dishId, rating, author, comment) => ({
@@ -11,3 +13,29 @@ export const addComment = (dishId, rating, author, comment) => ({
         comment: comment
     }
 });
+
+//this is a thunk - which returns a function
+export const fetchDishes =  () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+
+//function which return an action
+export const dishesLoading  = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+//function creator
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+//function creator
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
+})
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,21 @@
-import { DISHES } from '../shared/dishes';
+import *  as ActionTypes from './ActionTypes';
 
 //this is a pure function, as such you cannot modify its state directly in here
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: false, errMess: null, dishes: action.payload}; 
+
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMess: null, dishes: []}; //taking the original state, state is immutable
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload, dishes: []}; 
+
         default:
             return state; //no modification of the state, the state is simply returned
     }
